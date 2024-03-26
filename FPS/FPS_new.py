60eb0c9ff146b3e5a8cab96fbf4cdee3a83af8da
lienfeng <13146741545@163.com>
2024-03-26 11:18:02 +0800

    更新注释，优化代码

--- FPS/FPS_new.py
+++ FPS/FPS_new.py
@@ -1,4 +1,3 @@
-#%%
 import adbutils
 import threading
 import copy
@@ -8,17 +7,27 @@ import pathlib
 class check_FPS():
     def __init__(self,serial):
         self.adb = adbutils.AdbClient(host="127.0.0.1", port=5037)
+        #设备序列号
         self.serial = serial
+        #adb命令
         self.android = self.adb.device(serial)
+        #安卓版本
         self.version = self.android.shell("getprop ro.build.version.release")
+        #图层名称
         self.layer = None
+        #[时间，FPS]
         self.FPS = []
+        #上次记录的FPS
         self.old_FPS = 0
+        #手机型号
         self.model = self.android.shell('getprop ro.product.model')
+        #脚本开始时间
         self.start_time = int(time.time())
+        #初始化记录文件
         with open("{model}.csv".format(model=self.model),"w") as f:
             f.write("time,fps,screencap\n")
 
+    #获取图层名称
     def get_layer(self):
         SurfaceInfo = self.android.shell('dumpsys SurfaceFlinger')
         if self.version == "9":
@@ -43,6 +52,7 @@ class check_FPS():
         else:
             self.layer = layerNameBreak
 
+    #获取FPS
     def get_fps(self):
         command1 = self.android.shell('dumpsys SurfaceFlinger --latency "{layerName}"'.format(layerName=self.layer))
         Frames_temp = command1.split("\n")
@@ -55,6 +65,7 @@ class check_FPS():
         fps = 60//time_60
         return fps
 
+    #截图
     def screen_shot(self,num):
         png_data = self.android.shell("screencap -p", encoding=None)
         pathlib.Path("lowFPS{num}.png".format(num=num)).write_bytes(png_data)
@@ -82,13 +93,10 @@ class check_FPS():
 
 if __name__ == "__main__":
     adb = adbutils.AdbClient(host="127.0.0.1", port=5037)
-    devices = []
+    android_devices = []
     for i in adb.list():
         if i.state == 'device':
-            devices.append(i.serial)
-    android_devices =[]
-    for serial in devices:
-        android_devices.append(check_FPS(serial))
+            android_devices.append(check_FPS(i.serial))
     #初始化layername
     for mobile in android_devices:
         mobile.get_layer()
