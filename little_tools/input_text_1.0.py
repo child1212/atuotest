04d888a35c3fc2a1e332ce68626c5f690f9c3a53
lienfeng <13146741545@163.com>
2024-08-30 11:41:50 +0800

    调音量

--- little_tools/input_text_1.0.py
+++ little_tools/input_text_1.0.py
@@ -13,7 +13,7 @@ print("检测到{t}台设备：".format(t=len(ds)))
 threadPool = ThreadPoolExecutor(max_workers=len(ds),thread_name_prefix="test_")
 
 def clear_input(device):
-    for i in range(15):
+    for i in range(30):
         threading.Thread(target=device.shell,args=("input keyevent 67",)).start()
 
 while True:
@@ -23,14 +23,15 @@ while True:
         break
     elif cmd == "":
         for d in ds:
-            F = threadPool.submit(d.shell,"input keyevent 62")
+            F = threadPool.submit(d.shell,"input keyevent KEYCODE_ENTER")
     elif cmd == "del":
         for d in ds:
             F = threadPool.submit(d.shell,"input keyevent KEYCODE_MOVE_END")
             F = threadPool.submit(clear_input,d)
+    # elif ""
     else:    
         for d in ds:
-            F = threadPool.submit(d.send_keys,cmd)
+            F = threadPool.submit(d.shell,['input', 'text', cmd])
 
 
 
