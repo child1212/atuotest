5f56444698846b03e24a9c6be22464c0cbd7da86
lienfeng <13146741545@163.com>
2024-08-30 11:45:08 +0800

    新增脚本

--- little_tools/install.py
+++ little_tools/install.py
@@ -5,9 +5,14 @@ import threading
 ds = adb.device_list()
 print("检测到{n}台设备".format(n=len(ds)))
 p = input("apkpath:\n")
-for d in ds:
-    r = adb.device(d.serial)
-    threading.Thread(target=r.install,args=(p,)).start()
+if p == "test":
+    for d in ds:
+        r = adb.device(d.serial)
+        threading.Thread(target=r.shell,args=("input swipe 300 0 300 300",)).start()
+else:
+    for d in ds:
+        r = adb.device(d.serial)
+        threading.Thread(target=r.install,args=(p,)).start()
 
 
 
