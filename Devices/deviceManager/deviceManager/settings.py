14cccb4632a7b3ec4b8a292490a1b6d2f989ce19
lienfeng <13146741545@163.com>
2025-07-23 17:32:15 +0800

    删除虚拟环境

--- Devices/deviceManager/deviceManager/settings.py
+++ Devices/deviceManager/deviceManager/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cdjango_filters',
+    'django_filters',
     'catalog.apps.CatalogConfig',
 ]
 
